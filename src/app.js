f71c21e606a8c0a2d8c0a09ab95b100ecc626b71
kimCyton <2143039855@qq.com>
2017-02-26 22:17:11 +0800

    'handleScssloader'

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 import './css/common.css'
 import layer from './components/layer/layer.js';
-//import './components/layer/layer.scss';
+import './components/layer/layer.scss';
 const App = function(){
-	const NUM = 1;
-	alert(2)
 	console.log(layer)
 }
 
